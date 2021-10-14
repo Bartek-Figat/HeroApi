655bffdb80ff648ed76858be7718e9db9fda919a
Bartek-Figat <figat29@gmail.com>
2021-10-13 23:54:35 -0400

    init

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/',
     createProxyMiddleware({
-      target: 'https://hero-api-super-hero.vercel.app/',
+      target: 'https://hero-api-super-hero.vercel.app',
       changeOrigin: true,
     })
   );
