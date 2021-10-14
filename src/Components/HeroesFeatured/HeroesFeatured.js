07e9c95f9d8d701bc66ee801aa5800f98de41a3c
Bartek-Figat <figat29@gmail.com>
2021-10-13 23:07:29 -0400

    init

--- src/Components/HeroesFeatured/HeroesFeatured.js
+++ src/Components/HeroesFeatured/HeroesFeatured.js
@@ -1,42 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { getBasicHeroInfoById } from '../../Requests/requests';
-import HeroFeatured from './HeroFeatured/HeroFeatured';
-import Loader from '../Loader/Loader';
-import './HeroesFeatured.css';
+import React, { useEffect, useState } from "react";
+import { getBasicHeroInfoById } from "../../Requests/requests";
+import HeroFeatured from "./HeroFeatured/HeroFeatured";
+import Loader from "../Loader/Loader";
+import "./HeroesFeatured.css";
 
 function HeroesFeatured() {
   const [featuredHeroesList, setFeaturedHeroesList] = useState([]);
-  let [featuredHeroesIds, setFeaturedHeroesIds] = useState([1, 2, 3, 4]);
   const [isLoading, setLoadingState] = useState(true);
+  const [limit, setLimit] = useState(4);
 
   useEffect(() => {
     fetchAndDisplayFeaturedHeroes();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const generateRandomIds = () => {
-    setLoadingState(true);
-
-    let numberOfId;
-    featuredHeroesIds = [];
-
-    for (let i = 0; i < 4; i++) {
-      numberOfId = Math.floor(Math.random() * (731 - 0 + 1)) + 0; // Math.floor(Math.random() * (Max - Min + 1)) + Min
-      featuredHeroesIds.push(numberOfId);
-    }
-
-    setFeaturedHeroesIds(featuredHeroesIds);
-    fetchAndDisplayFeaturedHeroes();
-  };
-
   const fetchAndDisplayFeaturedHeroes = async () => {
-    let heroes = [];
-    for (const heroId of featuredHeroesIds) {
-      const data = await getBasicHeroInfoById(heroId); // {data} tell us, we only want to retrieve only object data from object || {data} mówi nam, ze chcemy pobrac tylko obiekt data z obiektu
-      heroes.push(data);
+    let herosList = [];
+    let numberOfId;
+    
+
+    for (let i = 0; i < limit; i++) {
+      numberOfId = Math.floor(Math.random() * (731 - 0 + 1)) + 0;
+      const data  = await getBasicHeroInfoById(numberOfId);
+      herosList.push(data)
     }
-    setFeaturedHeroesList(heroes);
-    setLoadingState(false);
+
+
+    if(!herosList){
+      setLoadingState(true);
+    }else{
+      setFeaturedHeroesList(herosList);
+      setLoadingState(false);
+    }
+
   };
 
   return (
@@ -45,11 +41,19 @@ function HeroesFeatured() {
         <h1>Featured Heroes</h1>
         {!isLoading && (
           <div className="featured__list">
-            {featuredHeroesList.map(({ name, imgUrl, powerstats, id }) => (
-              <HeroFeatured key={id} name={name} powerstats={powerstats} imgUrl={imgUrl} id={id} />
-            ))}
+            {Object.values(featuredHeroesList).map(
+              ({ name, imgUrl, powerstats, id }) => (
+                <HeroFeatured
+                  key={name}
+                  name={name}
+                  powerstats={powerstats}
+                  imgUrl={imgUrl}
+                  id={id}
+                />
+              )
+            )}
           </div>
-        )}{' '}
+        )}{" "}
         {isLoading && (
           <div className="loader-container">
             <Loader />
@@ -57,16 +61,19 @@ function HeroesFeatured() {
         )}
       </section>
 
-      <section className="featured__hero__refresh">
+      {/* <section className="featured__hero__refresh">
         {!isLoading && (
           <div className="container">
-            <button onClick={() => generateRandomIds()} className="featured__button">
+            <button
+              onClick={() => generateRandomIds()}
+              className="featured__button"
+            >
               Show new Featured Heroes
             </button>
           </div>
         )}
-        {isLoading && <></>}{' '}
-      </section>
+        {isLoading && <></>}{" "}
+      </section> */}
     </>
   );
 }
