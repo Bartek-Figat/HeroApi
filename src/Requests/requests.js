1717dbcb5d1ba291aff8c02ba02e3f89093e5668
Bartek-Figat <figat29@gmail.com>
2021-10-14 00:02:54 -0400

    init

--- src/Requests/requests.js
+++ src/Requests/requests.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
-
+const  config = {
+  headers: {'Access-Control-Allow-Origin': '*'}
+};
 export const getBasicHeroInfoById = async (id) => {
-  const { data: powerstats } = await axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}/powerstats`);
-  const { data: image } = await axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}/image`);
+  const { data: powerstats } = await axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}/powerstats`, config);
+  const { data: image } = await axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}/image`, config);
   return { name: powerstats.name, powerstats, imgUrl: image.url, id: powerstats.id };
 };
 
 export const getFullHeroInfoById = (id) => {
-  return axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}`);
+  return axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/${id}`, config);
 };
 
 export const searchHeroByName = (name) => {
-  return axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/search/${name}`);
+  return axios.get(`/api/${process.env.REACT_APP_HEROAPI_ACCESS_TOKEN}/search/${name}`, config);
 };
