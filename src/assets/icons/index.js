a9ee9c048bf9d76b2fb37078538423255372b190
Bartek-Figat <figat29@gmail.com>
2021-10-13 20:01:37 -0400

    init

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,7 +1,1 @@
-import durabilityIcon from './Durabilityicon.svg';
-import intelligenceIcon from './Intelligenceicon.svg';
-import powerIcon from './Powericon.svg';
-import speedIcon from './Speedicon.svg';
-import strengthIcon from './Strengthicon.svg';
 
-export { durabilityIcon, intelligenceIcon, powerIcon, speedIcon, strengthIcon };
